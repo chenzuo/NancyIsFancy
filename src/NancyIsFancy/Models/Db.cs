1211f6bc0fca6412cdd2870571b7fefce36aef7d
Jeff Doolittle <jeffdoolittle@gmail.com>
2014-06-19 11:26:04 -0700

    routing basics

---
+++ src/NancyIsFancy/Models/Db.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NancyIsFancy.Models
+{
+    public static class Db
+    {
+        static Db()
+        {
+            ProductStorage = new Dictionary<int, Product>();
+        }
+
+        private static int _nextProductIdentifier;
+        private static readonly Dictionary<int, Product> ProductStorage;
+        public static IQueryable<Product> Products { get { return ProductStorage.Values.AsQueryable(); } }
+
+        public static void Add(Product product)
+        {
+            if (product.Id == 0)
+            {
+                product.Id = ++_nextProductIdentifier;
+            }
+
+            ProductStorage[product.Id] = product;
+        }
+
+        public static void Remove(Product product)
+        {
+            if (!ProductStorage.ContainsKey(product.Id))
+            {
+                throw new DbException("Product not found with key " + product.Id);
+            }
+            ProductStorage.Remove(product.Id);
+        }
+    }
+
+    [Serializable]
+    public class DbException : Exception
+    {
+        public DbException(string message) : base(message)
+        {            
+        }
+    }
+}
---
+++ src/NancyIsFancy/Modules/ProductModule.cs
@@ -0,0 +1,101 @@
+﻿using System.Linq;
+using Nancy;
+using Nancy.ModelBinding;
+using NancyIsFancy.Models;
+
+namespace NancyIsFancy.Modules
+{
+    public class ProductModule : NancyModule
+    {
+        public ProductModule()
+        {
+            Get["/products/"] = _ =>
+                {
+                    var query = this.Bind<ProductQuery>();
+
+                    var queryable = Db.Products;
+
+                    if (!string.IsNullOrEmpty(query.Q))
+                    {
+                        queryable = queryable.Where(x => x.Name.ToLowerInvariant().Contains(query.Q.ToLowerInvariant()));
+                    }
+
+                    return queryable;
+                };
+
+            Get["/product/{id:int}"] = _ =>
+                {
+                    var id = (int)_.id;
+
+                    var product = Db.Products.SingleOrDefault(x => x.Id == id);
+
+                    if (product == null)
+                    {
+                        return HttpStatusCode.NotFound;
+                    }
+
+                    return product;
+                };
+
+            Post["/product"] = _ =>
+                {
+                    var model = this.Bind<ProductModel>();
+
+                    var product = new Product
+                        {
+                            Name = model.Name
+                        };
+
+                    Db.Add(product);
+
+                    return new
+                        {
+                            product.Id
+                        };
+                };
+
+            Put["/product/{id:int}"] = _ =>
+                {
+                    var id = (int)_.id;
+
+                    var model = this.Bind<ProductModel>();
+
+                    var product = new Product
+                    {
+                        Id = id,
+                        Name = model.Name
+                    };
+
+                    Db.Add(product);
+
+                    return HttpStatusCode.OK;
+                };
+
+            Delete["/product/{id:int}"] = _ =>
+                {
+                    var id = (int)_.id;
+
+                    var product = Db.Products.SingleOrDefault(x => x.Id == id);
+
+                    if (product == null)
+                    {
+                        return HttpStatusCode.NotFound;
+                    }
+
+                    Db.Remove(product);
+
+                    return HttpStatusCode.OK;
+                };
+        }
+
+        public class ProductModel
+        {
+            public string Name { get; set; }
+        }
+
+        public class ProductQuery
+        {
+            public string Q { get; set; }   
+        }
+    }
+}
